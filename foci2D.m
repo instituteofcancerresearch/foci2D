801c55d7772c1ad21af2d96879bddbc7547970f2
Adam Tyson <adamltyson@gmail.com>
2018-05-08 15:03:51 +0100

    Added cell by cell export

--- foci2D.m
+++ foci2D.m
@@ -1,4 +1,4 @@
-% function foci2D
+function foci2D
 %% Adam Tyson | 2018-05-03 | adam.tyson@icr.ac.uk
 % loads an .lsm file, segments the first channel (nuclear), and estimates
 % cell boundaries
@@ -15,11 +15,11 @@ cd(vars.directory)
 files=dir('*.lsm'); % all tif's in this folder
 numImages=length(files);
 imCount=0;
-
 f = waitbar(0,'1','Name','Analysing images...');
 for file=files' % go through all images
     imCount=imCount+1;
-    waitbar((imCount-1)/numImages,f,strcat("Analysing Image: ", num2str(imCount)))
+    waitbar((imCount-1)/numImages,f,strcat("Analysing Image: ",...
+                                            num2str(imCount)))
     
     rawFile{imCount}=file.name;
     disp(['Processing - ' rawFile{imCount}])
@@ -29,24 +29,84 @@ for file=files' % go through all images
     ch1Max=max(data.channel1,[],3);
     ch2Max=max(data.channel2,[],3);
     
+    % segment nuclei and find edges
     [labelDAPI, labelCell] = nucSegBorders(ch1Max, vars);
     
     % measure other channel
     [areaColoc{imCount}, intenColoc{imCount}, numFoci{imCount}] = ...
     fociPerCell(ch2Max, labelCell, labelDAPI, vars, rawFile{imCount});
-                                                                                   
+    % save results
 end
+
+if strcmp(vars.save, 'Yes')
+   save_raw_res(rawFile, numFoci, intenColoc, areaColoc, imCount, vars) 
+end
+
 delete(f)
 toc
-% end
+end
 
 %% Internal functions
+
+function save_raw_res(rawFile, numFoci, intenColoc,...
+                        areaColoc, imCount, vars)
+% tidy up
+
+for i=1:imCount
+lengths(i)=length(numFoci{i});
+end
+numFociReshape=NaN(imCount+1, max(lengths)+1);
+intenColocReshape=numFociReshape;
+areaColocReshape=numFociReshape;
+
+for i=1:imCount
+numFociReshape(i+1,2:length(numFoci{i})+1)=numFoci{i};
+intenColocReshape(i+1,2:length(intenColoc{i})+1)=intenColoc{i};
+areaColocReshape(i+1,2:length(areaColoc{i})+1)=areaColoc{i};
+end
+
+numFociCell=num2cell(numFociReshape);
+intenColocCell=num2cell(intenColocReshape);
+areaColocCell=num2cell(areaColocReshape);
+
+numFociCell{1,1}='Image';
+intenColocCell{1,1}='Image';
+areaColocCell{1,1}='Image';
+
+for cellnum=1:max(lengths)
+     numFociCell{1, cellnum+1}=strcat("Cell_", num2str(cellnum));
+     intenColocCell{1, cellnum+1}=strcat("Cell_", num2str(cellnum));
+     areaColocCell{1, cellnum+1}=strcat("Cell_", num2str(cellnum));
+ end
+ 
+for image=1:imCount
+     numFociCell{image+1,1}=rawFile{image};
+     intenColocCell{image+1,1}=rawFile{image};
+     areaColocCell{image+1,1}=rawFile{image};
+end
+
+fociNum_Table=cell2table(numFociCell);
+fociInten_Table=cell2table(intenColocCell);
+fociArea_Table=cell2table(areaColocCell);
+
+writetable(fociNum_Table, ['fociNumbers_' vars.stamp '.csv'],...
+                            'WriteVariableNames', 0)
+writetable(fociInten_Table, ['fociTotalInten_' vars.stamp '.csv'],...
+                            'WriteVariableNames', 0)
+writetable(fociArea_Table, ['fociTotalArea_' vars.stamp '.csv'],...
+                            'WriteVariableNames', 0)                        
+end
+
 function vars=getVars
 vars.directory = uigetdir('', 'Choose directory containing images');
 
-vars.plot = questdlg('Display results? ', ...
+vars.plot = questdlg('Display segmentation? ', ...
     'Plotting', ...
     'Yes', 'No', 'No');
+
+vars.save = questdlg('Save results as .csv? ', ...
+    'Saving', ...
+    'Yes', 'No', 'Yes');
 
 prompt = {'Nuclear segmentation threshold (a.u.):',...
     'Foci segmentation threshold (a.u.)::',...
@@ -66,4 +126,6 @@ vars.noiseRem=str2double(answer{4}); % smallest obj to remove
 vars.filtSigmaCh1=str2double(answer{5});% smoothing kernel
 vars.filtSigmaCh2=str2double(answer{6});% smoothing kernel
 
+vars.stamp=num2str(fix(clock)); % date and time 
+vars.stamp(vars.stamp==' ') = '';%remove spaces
 end
